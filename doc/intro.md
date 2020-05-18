71b8a9a8b3f1ee7cd529a8e1b826fb9e65496c45
Lei Wang <tendant@gmail.com>
2020-05-18 00:09:24 -0700

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to proxy-request
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/proxy_request/core_test.clj
@@ -0,0 +1,7 @@
+(ns proxy-request.core-test
+  (:require [clojure.test :refer :all]
+            [proxy-request.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
